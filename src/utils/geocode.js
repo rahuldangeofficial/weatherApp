24cfe01064592c929c23f1b00508a6c3f7e8ceb6
rahuldangeofficial <rahuldangeofficial@gmail.com>
2022-04-11 19:42:35 +0530

    Initial Commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request=require('request')
+
+const geocode=(address,callback)=>{
+    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoicmFodWxkYW5nZW9mZmljaWFsIiwiYSI6ImNsMWhsdDdicDFtY3AzcHAzOG9ydDNnOWoifQ.cbxO2TqLdwcXMB9jA9Jb2A&limit=1'
+
+    request({url:url,json:true},(error,response)=>{
+        if(error){
+            callback('Unable to connect to weather service!',undefined);
+        }else if(response.body.features.length===0){
+            callback('Unable to find location',undefined);
+        }else{
+            callback(undefined,{
+                latitude:response.body.features[0].center[1],
+                longitude:response.body.features[0].center[0],
+                location:response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports=geocode;
